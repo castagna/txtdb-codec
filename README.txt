54d62c1d09d822307185c75e38d6f49a9fa66415
Paolo Castagna <paolo.castagna@gmail.com>
2011-05-03 20:26:46 +0100

    Comments...

---
+++ README.txt
@@ -0,0 +1,17 @@
+TxTDB Codecs
+------------
+
+These are small experiments with codecs to compress Journal entries for TxTDB.
+
+I've stolen code and ideas from:
+
+ - Apache Avro [1]
+ - Tatu Saloranta's Ning Compress [2] and his benchmark [3]
+
+
+
+ [1] http://svn.apache.org/repos/asf/avro/trunk/lang/java/avro/src/main/java/org/apache/avro/file/Codec.java
+ [2] https://github.com/ning/compress
+ [3] https://github.com/ning/jvm-compressor-benchmark
+
+                                                            -- Paolo Castagna
--- src/test/java/dev/RunAdler32.java
+++ src/test/java/dev/RunAdler32.java
@@ -1,12 +1,25 @@
+/*
+* Copyright © 2011 Talis Systems Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package dev;
 
 import java.util.zip.Adler32;
 
 public class RunAdler32 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		byte[] b = new String("Hello World").getBytes();
 		Adler32 adler = new Adler32();
